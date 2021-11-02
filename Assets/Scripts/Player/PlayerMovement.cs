1cd5b5a462ec79d4cea354d647346e451c7e88f9
PoxhYT <72230632+PoxhYT@users.noreply.github.com>
2021-11-02 16:10:39 +0100

    Added SetupControlls function to PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;    
+    public Rigidbody rb;
+    public float forceSpeed = 500;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        AddForceToRigidbody(0, 0, 2000);    
+        AddForceToRigidbody(0, 0, forceSpeed * Time.deltaTime);
+        SetupControlls();
     }
 
-    private void AddForceToRigidbody(int valueA, int valueB, int valueC)
+    private void AddForceToRigidbody(float valueA, float valueB, float valueC)
     {
-        Vector3 force = new Vector3(valueA, valueB, valueC * Time.deltaTime);
+        Vector3 force = new Vector3(valueA, valueB, valueC);
         rb.AddForce(force);
+    }
+
+    private void SetupControlls()
+    {
+        if (Input.GetKey("d"))
+        {
+            AddForceToRigidbody(1000 * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("a"))
+        {
+            AddForceToRigidbody(-1000 * Time.deltaTime, 0, 0);
+        }
     }
 }
