195a4ddcbca1e6b107bf8fcecf921869cd6b1126
PoxhYT <72230632+PoxhYT@users.noreply.github.com>
2021-11-03 18:21:59 +0100

    Added restartGame at UIManager and fixed lightning after game restart

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void restartGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Level01");
     }
 }
